e3441c4dc3681f5e5b907965f86398562c53f2a5
Tri Van BUI <trivbuit@gmail.com>
2021-03-29 16:01:53 +0700

    Add count

--- TF-AWS/2-demo/main.tf
+++ TF-AWS/2-demo/main.tf
@@ -23,7 +23,7 @@ terraform {
 module "ec2_cluster" {
   source = "github.com/trivbuit/trModule-Terraform-AWS-EC2-Instance?ref=v1.0.4"
 
-  count = 2
+  count = 3
 
   ami           = "ami-0f86a70488991335e"
   instance_type = "t2.micro"
