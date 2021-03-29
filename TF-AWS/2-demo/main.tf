51b20e2fc5b6c0991bf0a75593ae4ff6b317057a
Tri Van BUI <trivbuit@gmail.com>
2021-03-29 15:02:56 +0700

    Main version

--- TF-AWS/2-demo/main.tf
+++ TF-AWS/2-demo/main.tf
@@ -1,19 +1,8 @@
 
-terraform {
-  backend "remote" {
-    hostname = "trivbuit.scalr.io"
-    organization = "env-tct37d1oddo7u4g"
-
-    workspaces {
-      name = "Local2ScalrPro"
-    }
-  }
-}
-
 module "ec2_cluster" {
   source = "github.com/trivbuit/trModule-Terraform-AWS-EC2-Instance?ref=v1.0.4"
 
-  count = 3
+  count = 1
 
   ami           = "ami-0f86a70488991335e"
   instance_type = "t2.micro"
