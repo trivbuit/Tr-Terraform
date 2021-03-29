b1a9febba0ea1125f4de653d6e08aff4d4e76bb7
Tri Van BUI <trivbuit@gmail.com>
2021-03-29 18:41:17 +0700

    Update migrate code to Scalr

---
+++ TF-AWS/4-demo2/main.tf
@@ -0,0 +1,36 @@
+# Configure the AWS Provider
+provider "aws" {
+  region     = "ap-southeast-1"
+}
+
+# terraform {
+#   backend "s3" {
+#     bucket     = "rt-tf-test-bucket"
+#     key        = "terraform.tfstate"
+#     region     = "ap-southeast-1"
+#     access_key = "access_key"
+#     secret_key = "secret_key"
+#   }
+# }
+
+terraform {
+  backend "remote" {
+    hostname = "trivbuit.scalr.io"
+    organization = "env-tct37d1oddo7u4g"
+
+    workspaces {
+      name = "S3_2_Scalr"
+    }
+  }
+}
+
+module "ec2_cluster" {
+  source = "github.com/trivbuit/trModule-Terraform-AWS-EC2-Instance?ref=v1.0.4"
+
+  count = 3
+
+  ami           = "ami-0f86a70488991335e"
+  instance_type = "t2.micro"
+
+  key_name = "trbvm"
+}
